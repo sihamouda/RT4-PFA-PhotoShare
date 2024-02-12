900982109714b26ddda26dab30e853133235e0b0
sihamouda <anis.c.hamouda@gmail.com>
2024-02-12 14:55:21 +0100

    feat: connect postgres

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    // TODO: impove config logic
+    ConfigModule.forRoot({
+      envFilePath: '../../.dev.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('POSTGRES_HOST'),
+          port: configService.get<number>('POSTGRES_PORT'),
+          username: configService.get<string>('POSTGRES_USER'),
+          password: configService.get<string>('POSTGRES_PASSWORD'),
+          database: configService.get<string>('POSTGRES_DB'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
