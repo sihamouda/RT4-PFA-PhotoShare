02956b23c72f920d741308db82c3c61cefa01bab
sihamouda <anis.c.hamouda@gmail.com>
2024-06-03 21:55:30 +0100

    fix: typeorm module import

--- apps/payment/src/app.module.ts
+++ apps/payment/src/app.module.ts
@@ -4,17 +4,38 @@ import { AppService } from './app.service';
 import { PaymentController } from './payment/payment.controller';
 import { PaymentModule } from './payment/payment.module';
 import { PaymentService } from './payment/payment.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { ConsulModule } from 'common';
 import { name } from '../package.json';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PaymentModule, HttpModule , ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '../../.env.dev',
-
-  }),ConsulModule.register(name)],
+  imports: [
+    PaymentModule,
+    HttpModule,
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities:
+          configService.get<string>('NODE_ENV') === 'local' ? true : false,
+        synchronize:
+          configService.get<string>('NODE_ENV') === 'local' ? true : false,
+      }),
+      inject: [ConfigService],
+    }),
+    ConsulModule.register(name),
+  ],
   controllers: [AppController, PaymentController],
   providers: [AppService, PaymentService],
 })
